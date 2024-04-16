21d069711d36d1ca230f1f3c053f2444c9889e0c
anna <silver-annazilberberg@yandex.ru>
2024-04-16 13:17:46 +0700

    добавлено изменение контекста для дочернего кнотекста

---
+++ cmdargs.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+int main (int argc, char *argv[], char *envp[] ) {
+	int i;
+       	printf("Количество аргументов: %d\n\n", argc);		
+	 if (argc<1) {
+		printf("ошибка\n");
+		exit(-1);
+ 	 }
+	 else if (argc ==1) {
+		while (envp[i]!=NULL) {
+	                printf("%s\n", envp[i]);
+	                i++;
+                }
+
+         }
+	 else{
+		for (i=0; i<argc; i++) {
+		printf("Аргумент %d: %s\n", i, argv[i]);
+       	}
+
+	return 0;
+}
+
