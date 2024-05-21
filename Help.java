10a9d5bc0959fcdcffba955cb204201d5f8bcdbc
anna <silver-annazilberberg@yandex.ru>
2024-05-21 13:33:09 +0700

    Добавили бесконечный цикл в работу программыи выход по клавише q

--- Help.java
+++ Help.java
@@ -4,6 +4,8 @@ class Help {
 		
 		char choice, ignore;
 		
+		for(;;) {
+		
 		do {	
 			System.out.println("Справочная система:");
 			System.out.println(" 1. if");
@@ -11,13 +13,16 @@ class Help {
 			System.out.println(" 3. for");
                         System.out.println(" 4. while");
 			System.out.println(" 5. do-while");
+			System.out.println("q - выход из программы");
 			System.out.println("Выберите подраздел: ");
 			choice = (char) System.in.read();
 			do {
 				ignore = (char) System.in.read();
 			} while (ignore != '\n');
-		} while (choice <'1' | choice > '5');
+		} while (choice <'1' | choice > '5' & choice !='q');
 		System.out.println("\n");
+
+		if (choice == 'q') break;
 
 		switch (choice) {
 			case '1':
@@ -50,8 +55,8 @@ class Help {
 				System.out.println("} while (условие);");
                                 break;
 
-
-
+		}
+		System.out.println();
 		}
 	}
 }
