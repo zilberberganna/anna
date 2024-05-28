15bd6db6ef8a59ddcd54223958bbbc27ef952299
anna <silver-annazilberberg@yandex.ru>
2024-05-28 11:17:20 +0700

    Добавили новую информацию об автомобилях

--- Obj1.java
+++ Obj1.java
@@ -31,6 +31,18 @@ class Obj1 {
 		System.out.println("Емкость топливного бака: " + nissan.vfuel);
 		System.out.println("Тип кузова: " + nissan.type);
 		nissan.range();
+		System.out.println("Информация об автомобиле \"Форд\"");
+                System.out.println("Количество пассажиров: " + ford.kpass);
+                System.out.println("Емкость топливного бака: " + ford.vfuel);
+                System.out.println("Тип кузова: " + ford.type);
+                ford.range();
+		System.out.println("Информация об автомобиле \"БМВ\"");
+                System.out.println("Количество пассажиров: " + bmw.kpass);
+                System.out.println("Емкость топливного бака: " + bmw.vfuel);
+                System.out.println("Тип кузова: " + bmw.type);
+                bmw.range();
+
+
 	}
 
 }
