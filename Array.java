b7c573647e036652c3be93293aad440a81a243c2
anna <silver-annazilberberg@yandex.ru>
2024-06-04 11:35:31 +0700

    Добавили использование переменной length

--- Array.java
+++ Array.java
@@ -8,19 +8,19 @@ class Array {
 		for(i = 0; i < 10; i++)
 			System.out.println("Элемент [" + i + "]: " + nums[i]);
 		//Альтернативный способ инициализации массива
-		int nums2[] = {99, -50, -245, 311, -50, 18, 90, 84, -4, 2};
+		int nums2[] = {99, -50, -245, 311, -50, 18, 90, 84, -4, 2, 3434, 1414, -343, -1};
 
 		int min, max; 
 		//поиск минимамального и максимального значений
 		min = max = nums2[0];
-		for (i = 1; i < 10; i++) {
+		for (i = 1; i < nums2.length; i++) {
 			if(nums2[i] < min) min = nums2[i];
 			if(nums2[i] > max) max = nums2[i];
 		}
 		System.out.println("Минимальное значение в массиве: " + min + "\n Максимальное значение в массиве: " + max);
 		//Пузырьковая сортировка массива numss
 		int a, b, t;
-		int size = 10;
+		int size = nums2.length;
 	        System.out.println("Массив до сортировки: ");
 		for (i = 0; i < size; i++)
 			System.out.println(" " + nums2[i]);
@@ -37,5 +37,11 @@ class Array {
 		System.out.println("Массив после сортировки: ");
 		for ( i = 0; i < size; i++)
 			System.out.println(" " + nums2[i]);
+		//Использование переменной экземпляра length
+		System.out.println("Размер nums: " + nums.length);
+		System.out.println("Размер nums2: " + nums2.length);
+		for ( i = 0; i < nums2.length; i++)
+			System.out.println(nums2[i]);
+
 	}
 }
