d31d9858e79559d33df8d28d8743c085cd0ceefa
anna <silver-annazilberberg@yandex.ru>
2024-06-04 10:36:04 +0700

    Добвили параметризированный конструктор в программу Obj1.java

--- Obj1.java
+++ Obj1.java
@@ -1,8 +1,17 @@
-`class Avto {
+class Avto {
 	int kpass;
 	int vfuel;
 	double rash;
 	String type;
+	
+	Avto(int kp, int vf, double r, String t) {
+       //		System.out.println("Выполнился конструктор");
+		kpass=kp;
+		vfuel=vf;
+		rash=r;
+		type=t;
+	}
+
 	
 	void range() {
 		System.out.println("Автомобиль проедет " + vfuel/rash*100 + "км. на полном баке");
@@ -16,21 +25,21 @@
 }
 class Obj1 {
 	public static void main(String args[]) {
-		Avto nissan = new Avto();
-		Avto ford = new Avto();
-		Avto bmw = new Avto();	
-		nissan.kpass = 7;
-		ford.kpass = 5;
-		bmw.kpass = 2;
-		nissan.type = "Минивэн";
-		ford.type = "Седан";
-		bmw.type = "Кабриолет";
-		nissan.vfuel = 50;
-		ford.vfuel = 40;
-		bmw.vfuel = 20;
-		nissan.rash = 12.5;
-		ford.rash = 10.2;
-		bmw.rash = 18.5;
+		Avto nissan = new Avto(7, 50, 12.5, "Минивэн");
+		Avto ford = new Avto(5, 40, 10.2, "Седан");
+		Avto bmw = new Avto(2, 20, 18.5, "Кабриолет");	
+	//	nissan.kpass = 7;
+	//	ford.kpass = 5;
+	//	bmw.kpass = 2;
+	//	nissan.type = "Минивэн";
+	//	ford.type = "Седан";
+	//	bmw.type = "Кабриолет";
+	//	nissan.vfuel = 50;
+	//	ford.vfuel = 40;
+	//	bmw.vfuel = 20;
+	//	nissan.rash = 12.5;
+	//	ford.rash = 10.2;
+	//	bmw.rash = 18.5;
 		System.out.println("Информация об автомобиле \"Ниссан\"");
 		System.out.println("Количество пассажиров: " + nissan.kpass);
 		System.out.println("Емкость топливного бака: " + nissan.vfuel);
