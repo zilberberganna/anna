0613048739d636b115ab001a3d7507b81f53ac26
zilberberganna <167157649+zilberberganna@users.noreply.github.com>
2024-06-20 18:50:18 +0700

    Update Resistance.java

--- Resistance.java
+++ Resistance.java
@@ -1,90 +1,172 @@
-class ActiveResistance {
+// Создаем класс activeresistance
+
+class activeresistance {
+
     private double resistance;
 
-    public ActiveResistance() {
+    // Конструктор без параметров
+
+    public activeresistance() {
+
         resistance = 0;
+
     }
 
-    public ActiveResistance(double r) {
+    // Конструктор с параметром
+
+    public activeresistance(double r) {
+
         resistance = r;
+
     }
 
-    public double getResistance() {
+    // Геттер для resistance
+
+    public double getresistance() {
+
         return resistance;
+
     }
 
-    public void setResistance(double r) {
+    // Сеттер для resistance
+
+    public void setresistance(double r) {
+
         resistance = r;
+
     }
 
-    void showResistance() {
-        System.out.println("Активное сопротивление: " + resistance + " Ом");
+    // Метод для вывода активного сопротивления
+
+    void showresistance() {
+
+        System.out.println("активное сопротивление: " + resistance + " ом");
+
     }
+
 }
 
-class InductiveResistance extends ActiveResistance {
+// Создаем класс inductiveresistance, наследуемый от activeresistance
+
+class inductiveresistance extends activeresistance {
+
     private double inductance;
 
-    public double getTotalResistance() {
-        return Math.sqrt(Math.pow(getResistance(), 2) + Math.pow(inductance, 2));
+    // Метод для расчета общего сопротивления (с учетом индуктивности)
+
+    public double gettotalresistance() {
+
+        return Math.sqrt(Math.pow(getresistance(), 2) + Math.pow(inductance, 2));
+
     }
 
-    public double getInductance() {
+    // Геттер для inductance
+
+    public double getinductance() {
+
         return inductance;
+
     }
 
-    public void setInductance(double i) {
+    // Сеттер для inductance
+
+    public void setinductance(double i) {
+
         inductance = i;
+
     }
 
-    void showInductance() {
-        System.out.println("Индуктивное сопротивление: " + inductance + " Ом");
+    // Метод для вывода индуктивного сопротивления
+
+    void showinductance() {
+
+        System.out.println("индуктивное сопротивление: " + inductance + " ом");
+
     }
+
 }
 
-class CapacitiveResistance extends ActiveResistance {
+// Создаем класс capacitiveresistance, наследуемый от activeresistance
+
+class capacitiveresistance extends activeresistance {
+
     private double capacitance;
 
-    public double getTotalResistance() {
-        return 1 / Math.sqrt(Math.pow(getResistance(), 2) + Math.pow(1 / capacitance, 2));
+    // Метод для расчета общего сопротивления (с учетом емкости)
+
+    public double gettotalresistance() {
+
+        return 1 / Math.sqrt(Math.pow(getresistance(), 2) + Math.pow(1 / capacitance, 2));
+
     }
 
-    public double getCapacitance() {
+    // Геттер для capacitance
+
+    public double getcapacitance() {
+
         return capacitance;
+
     }
 
-    public void setCapacitance(double c) {
+    // Сеттер для capacitance
+
+    public void setcapacitance(double c) {
+
         capacitance = c;
+
     }
 
-    void showCapacitance() {
-        System.out.println("Емкостное сопротивление: " + capacitance + " Ф");
+    // Метод для вывода емкостного сопротивления
+
+    void showcapacitance() {
+
+        System.out.println("емкостное сопротивление: " + capacitance + " ф");
+
     }
+
 }
 
-class Resistors {
+// Создаем класс resistors
+
+class resistors {
+
     public static void main(String args[]) {
-        ActiveResistance r1 = new ActiveResistance(10.0);
-        InductiveResistance r2 = new InductiveResistance();
-        CapacitiveResistance r3 = new CapacitiveResistance();
 
-        r2.setResistance(5.0);
-        r2.setInductance(2.0);
+        activeresistance r1 = new activeresistance(10.0);
 
-        r3.setResistance(3.0);
-        r3.setCapacitance(0.5);
+        inductiveresistance r2 = new inductiveresistance();
 
-        System.out.println("Информация об r1: ");
-        r1.showResistance();
+        capacitiveresistance r3 = new capacitiveresistance();
 
-        System.out.println("Информация об r2: ");
-        r2.showResistance();
-        r2.showInductance();
-        System.out.println("Общее сопротивление: " + r2.getTotalResistance() + " Ом");
+        r2.setresistance(5.0);
 
-        System.out.println("Информация об r3: ");
-        r3.showResistance();
-        r3.showCapacitance();
-        System.out.println("Общее сопротивление: " + r3.getTotalResistance() + " Ом");
+        r2.setinductance(2.0);
+
+        r3.setresistance(3.0);
+
+        r3.setcapacitance(0.5);
+
+        System.out.println("информация об r1: ");
+
+        r1.showresistance();
+
+        System.out.println("информация об r2: ");
+
+        r2.showresistance();
+
+        r2.showinductance();
+
+        System.out.println("общее сопротивление: " + r2.gettotalresistance() + " ом");
+
+        System.out.println("информация об r3: ");
+
+        r3.showresistance();
+
+        r3.showcapacitance();
+
+        System.out.println("общее сопротивление: " + r3.gettotalresistance() + " ом");
+
     }
+
 }
+
